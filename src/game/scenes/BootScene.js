81bae80ec38a40d6a7471a8f3d3ac2866a228b8f
RubenMR99 <ruben.moreno.romero@gmail.com>
2019-04-01 17:26:58 +0200

    menu implementat

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -1,6 +1,7 @@
 import {Scene} from 'phaser'
 import sky from '@/game/assets/sky.png';
 import bomb from '@/game/assets/bomb.png';
+import boto from '@/game/assets/botoPlaceholder.png';
 
 export default class BootScene extends Scene {
     constructor() {
@@ -10,10 +11,33 @@ export default class BootScene extends Scene {
     preload() {
         this.load.image('sky', sky)
         this.load.image('bomb', bomb)
+		this.load.image('boto', boto)
         // this.load.audio('thud', ['assets/thud.mp3', 'assets/thud.ogg'])
     }
 
-    create() {
-        this.scene.start('PlayScene')
+    create() {		
+		let boto1 = this.add.sprite(400, 300, 'boto').setInteractive();
+		this.add.text(250, 280, 'Play Game', { font: '25px Arial Bold', fill: 'Black' });
+		let th = this
+		
+		boto1.on('pointerdown', function (pointer) {
+			
+			this.setTint(0xff0000);
+			th.scene.start('PlayScene');
+
+		});
+
+		boto1.on('pointerout', function (pointer) {
+
+			this.clearTint();
+
+		});
+
+		boto1.on('pointerup', function (pointer) {
+
+			this.clearTint();
+
+		});
     }
+
 }
--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -1,5 +1,6 @@
 import { Scene } from 'phaser';
 
+let bomb;
 
 export default class PlayScene extends Scene {
   constructor () {
@@ -9,16 +10,23 @@ export default class PlayScene extends Scene {
   create () {
     console.log("Starting PlayScene ...");
     let i = this.add.image(400, 300, 'sky');
-    console.log(i);
 
-    const bomb = this.physics.add.image(400, 200, 'bomb');
+    bomb = this.physics.add.image(400, 200, 'bomb');
     bomb.setCollideWorldBounds(true);
     bomb.body.onWorldBounds = true; // enable worldbounds collision event
     bomb.setBounce(1);
     bomb.setVelocity(200, 20);
-	this.add.text(10, 10, bomb.body.position.x{ font: '48px Arial', fill: '#000000' });
+	 //  By default the Signal is empty, so we create it here:
+    //bomb.body.onWorldBounds = new Phaser.Signal();
+
+    //  And then listen for it
+	//bomb.body.onWorldBounds.add(hitWorldBounds, this);
+	//this.add.text(10, 10, bomb.body.position.x{ font: '48px Arial', fill: '#000000' });
+	console.log(this.physics.bomb); //.bomb.body.position.x
   }
 
   update () {
+	 //console.log(bomb.body.onWorldBounds)
   }
+    
 }
